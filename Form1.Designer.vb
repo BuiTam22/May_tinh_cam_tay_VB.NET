0026c0b079e95f9d5214386148133849d5f1ef24
BuiTam22 <123612644+BuiTam22@users.noreply.github.com>
2023-04-24 22:49:01 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,419 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnsin = New System.Windows.Forms.Button()
+        Me.btnbang = New System.Windows.Forms.Button()
+        Me.btnCham = New System.Windows.Forms.Button()
+        Me.btncb2 = New System.Windows.Forms.Button()
+        Me.btnabs = New System.Windows.Forms.Button()
+        Me.btnChamThan = New System.Windows.Forms.Button()
+        Me.btnpow = New System.Windows.Forms.Button()
+        Me.btncos = New System.Windows.Forms.Button()
+        Me.btnchia = New System.Windows.Forms.Button()
+        Me.btnnhan = New System.Windows.Forms.Button()
+        Me.btntru = New System.Windows.Forms.Button()
+        Me.btncong = New System.Windows.Forms.Button()
+        Me.btnMOD = New System.Windows.Forms.Button()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.btnoff = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(219, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(377, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Chương trình máy tính bỏ túi"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(529, 146)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(322, 45)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn0.Location = New System.Drawing.Point(62, 440)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(69, 65)
+        Me.btn0.TabIndex = 3
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(62, 346)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(71, 65)
+        Me.btn1.TabIndex = 4
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(210, 346)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(69, 65)
+        Me.btn2.TabIndex = 5
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(359, 340)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(69, 65)
+        Me.btn3.TabIndex = 6
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(62, 244)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(71, 63)
+        Me.btn4.TabIndex = 7
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(210, 240)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(69, 63)
+        Me.btn5.TabIndex = 8
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(359, 244)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(69, 63)
+        Me.btn6.TabIndex = 9
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(62, 132)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(69, 59)
+        Me.btn7.TabIndex = 10
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(210, 132)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(69, 59)
+        Me.btn8.TabIndex = 11
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(359, 132)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(71, 59)
+        Me.btn9.TabIndex = 12
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnsin
+        '
+        Me.btnsin.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnsin.Location = New System.Drawing.Point(210, 440)
+        Me.btnsin.Name = "btnsin"
+        Me.btnsin.Size = New System.Drawing.Size(69, 65)
+        Me.btnsin.TabIndex = 13
+        Me.btnsin.Text = "sin"
+        Me.btnsin.UseVisualStyleBackColor = True
+        '
+        'btnbang
+        '
+        Me.btnbang.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnbang.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnbang.Location = New System.Drawing.Point(714, 446)
+        Me.btnbang.Name = "btnbang"
+        Me.btnbang.Size = New System.Drawing.Size(118, 59)
+        Me.btnbang.TabIndex = 14
+        Me.btnbang.Text = "="
+        Me.btnbang.UseVisualStyleBackColor = False
+        '
+        'btnCham
+        '
+        Me.btnCham.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCham.Location = New System.Drawing.Point(359, 440)
+        Me.btnCham.Name = "btnCham"
+        Me.btnCham.Size = New System.Drawing.Size(74, 65)
+        Me.btnCham.TabIndex = 15
+        Me.btnCham.Text = "."
+        Me.btnCham.UseVisualStyleBackColor = True
+        '
+        'btncb2
+        '
+        Me.btncb2.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btncb2.Location = New System.Drawing.Point(62, 555)
+        Me.btncb2.Name = "btncb2"
+        Me.btncb2.Size = New System.Drawing.Size(69, 59)
+        Me.btncb2.TabIndex = 16
+        Me.btncb2.Text = "cb2"
+        Me.btncb2.UseVisualStyleBackColor = True
+        '
+        'btnabs
+        '
+        Me.btnabs.Location = New System.Drawing.Point(480, 443)
+        Me.btnabs.Name = "btnabs"
+        Me.btnabs.Size = New System.Drawing.Size(69, 59)
+        Me.btnabs.TabIndex = 17
+        Me.btnabs.Text = "|X|"
+        Me.btnabs.UseVisualStyleBackColor = True
+        '
+        'btnChamThan
+        '
+        Me.btnChamThan.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnChamThan.Location = New System.Drawing.Point(359, 555)
+        Me.btnChamThan.Name = "btnChamThan"
+        Me.btnChamThan.Size = New System.Drawing.Size(71, 59)
+        Me.btnChamThan.TabIndex = 18
+        Me.btnChamThan.Text = "!"
+        Me.btnChamThan.UseVisualStyleBackColor = True
+        '
+        'btnpow
+        '
+        Me.btnpow.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnpow.Location = New System.Drawing.Point(480, 555)
+        Me.btnpow.Name = "btnpow"
+        Me.btnpow.Size = New System.Drawing.Size(69, 59)
+        Me.btnpow.TabIndex = 19
+        Me.btnpow.Text = "^"
+        Me.btnpow.UseVisualStyleBackColor = True
+        '
+        'btncos
+        '
+        Me.btncos.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btncos.Location = New System.Drawing.Point(210, 555)
+        Me.btncos.Name = "btncos"
+        Me.btncos.Size = New System.Drawing.Size(69, 59)
+        Me.btncos.TabIndex = 20
+        Me.btncos.Text = "cos"
+        Me.btncos.UseVisualStyleBackColor = True
+        '
+        'btnchia
+        '
+        Me.btnchia.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnchia.Location = New System.Drawing.Point(596, 344)
+        Me.btnchia.Name = "btnchia"
+        Me.btnchia.Size = New System.Drawing.Size(76, 65)
+        Me.btnchia.TabIndex = 21
+        Me.btnchia.Text = "/"
+        Me.btnchia.UseVisualStyleBackColor = True
+        '
+        'btnnhan
+        '
+        Me.btnnhan.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnnhan.Location = New System.Drawing.Point(598, 244)
+        Me.btnnhan.Name = "btnnhan"
+        Me.btnnhan.Size = New System.Drawing.Size(74, 63)
+        Me.btnnhan.TabIndex = 22
+        Me.btnnhan.Text = "*"
+        Me.btnnhan.UseVisualStyleBackColor = True
+        '
+        'btntru
+        '
+        Me.btntru.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btntru.Location = New System.Drawing.Point(480, 344)
+        Me.btntru.Name = "btntru"
+        Me.btntru.Size = New System.Drawing.Size(69, 65)
+        Me.btntru.TabIndex = 23
+        Me.btntru.Text = "-"
+        Me.btntru.UseVisualStyleBackColor = True
+        '
+        'btncong
+        '
+        Me.btncong.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btncong.Location = New System.Drawing.Point(480, 244)
+        Me.btncong.Name = "btncong"
+        Me.btncong.Size = New System.Drawing.Size(69, 63)
+        Me.btncong.TabIndex = 24
+        Me.btncong.Text = "+"
+        Me.btncong.UseVisualStyleBackColor = True
+        '
+        'btnMOD
+        '
+        Me.btnMOD.Location = New System.Drawing.Point(596, 446)
+        Me.btnMOD.Name = "btnMOD"
+        Me.btnMOD.Size = New System.Drawing.Size(76, 59)
+        Me.btnMOD.TabIndex = 25
+        Me.btnMOD.Text = "MOD"
+        Me.btnMOD.UseVisualStyleBackColor = True
+        '
+        'Button24
+        '
+        Me.Button24.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button24.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button24.Location = New System.Drawing.Point(714, 244)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(118, 63)
+        Me.Button24.TabIndex = 26
+        Me.Button24.Text = "Xóa trắng"
+        Me.Button24.UseVisualStyleBackColor = False
+        '
+        'btnoff
+        '
+        Me.btnoff.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnoff.Location = New System.Drawing.Point(596, 556)
+        Me.btnoff.Name = "btnoff"
+        Me.btnoff.Size = New System.Drawing.Size(118, 65)
+        Me.btnoff.TabIndex = 27
+        Me.btnoff.Text = "OFF"
+        Me.btnoff.UseVisualStyleBackColor = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(529, 77)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(322, 46)
+        Me.TextBox2.TabIndex = 28
+        '
+        'btnXoa
+        '
+        Me.btnXoa.BackColor = System.Drawing.Color.Orange
+        Me.btnXoa.ForeColor = System.Drawing.Color.Black
+        Me.btnXoa.Location = New System.Drawing.Point(714, 344)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(118, 65)
+        Me.btnXoa.TabIndex = 29
+        Me.btnXoa.Text = "Xóa"
+        Me.btnXoa.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Cornsilk
+        Me.ClientSize = New System.Drawing.Size(863, 671)
+        Me.Controls.Add(Me.btnXoa)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.btnoff)
+        Me.Controls.Add(Me.Button24)
+        Me.Controls.Add(Me.btnMOD)
+        Me.Controls.Add(Me.btncong)
+        Me.Controls.Add(Me.btntru)
+        Me.Controls.Add(Me.btnnhan)
+        Me.Controls.Add(Me.btnchia)
+        Me.Controls.Add(Me.btncos)
+        Me.Controls.Add(Me.btnpow)
+        Me.Controls.Add(Me.btnChamThan)
+        Me.Controls.Add(Me.btnabs)
+        Me.Controls.Add(Me.btncb2)
+        Me.Controls.Add(Me.btnCham)
+        Me.Controls.Add(Me.btnbang)
+        Me.Controls.Add(Me.btnsin)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MÁY TÍNH BỎ TÚI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnsin As Button
+    Friend WithEvents btnbang As Button
+    Friend WithEvents btnCham As Button
+    Friend WithEvents btncb2 As Button
+    Friend WithEvents btnabs As Button
+    Friend WithEvents btnChamThan As Button
+    Friend WithEvents btnpow As Button
+    Friend WithEvents btncos As Button
+    Friend WithEvents btnchia As Button
+    Friend WithEvents btnnhan As Button
+    Friend WithEvents btntru As Button
+    Friend WithEvents btncong As Button
+    Friend WithEvents btnMOD As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents btnoff As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnXoa As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,246 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim A As Double = 0, B As Double = 0, pheptinh As String, kq As Double = 0
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles btnoff.Click
+        End
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+        TextBox1.Text += "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+        TextBox1.Text += "1"
+
+
+
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+        TextBox1.Text += "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+        TextBox1.Text += "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+        TextBox1.Text += "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+        TextBox1.Text += "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+        TextBox1.Text += "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+        TextBox1.Text += "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+        TextBox1.Text += "8"
+    End Sub
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If TextBox2.Text.EndsWith("=") Then
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+        TextBox1.Text += "9"
+    End Sub
+
+    Private Sub btncong_Click(sender As Object, e As EventArgs) Handles btncong.Click
+
+        TextBox2.Text = TextBox1.Text + "+"
+        A = TextBox1.Text
+        TextBox1.Text = ""
+        pheptinh = "+"
+    End Sub
+
+    Private Sub btntru_Click(sender As Object, e As EventArgs) Handles btntru.Click
+        If TextBox1.Text <> "" Then
+            TextBox2.Text = TextBox1.Text + "-"
+            A = TextBox1.Text
+            TextBox1.Text = ""
+            pheptinh = "-"
+        Else
+            TextBox1.Text += "-"
+        End If
+    End Sub
+
+    Private Sub btnnhan_Click(sender As Object, e As EventArgs) Handles btnnhan.Click
+        TextBox2.Text = TextBox1.Text + "*"
+        A = TextBox1.Text
+        TextBox1.Text = ""
+        pheptinh = "*"
+    End Sub
+
+    Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
+        If TextBox1.Text.Length > 0 Then
+            TextBox1.Text = (TextBox1.Text).Remove((TextBox1.Text).Length - 1, 1)
+        Else
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnChamThan_Click(sender As Object, e As EventArgs) Handles btnChamThan.Click
+        A = TextBox1.Text
+        TextBox2.Text = TextBox1.Text + "!"
+        kq = 1
+        For i As Integer = 1 To A
+            kq *= i
+        Next
+        TextBox1.Text = kq
+    End Sub
+
+    Private Sub btnsin_Click(sender As Object, e As EventArgs) Handles btnsin.Click
+        TextBox2.Text = "sin("
+        pheptinh = "sin"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btncos_Click(sender As Object, e As EventArgs) Handles btncos.Click
+        TextBox2.Text = "cos("
+        pheptinh = "cos"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btncb2_Click(sender As Object, e As EventArgs) Handles btncb2.Click
+        TextBox2.Text = "sqrt("
+        pheptinh = "sqrt"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btnabs_Click(sender As Object, e As EventArgs) Handles btnabs.Click
+        TextBox2.Text = "abs("
+        pheptinh = "abs"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btnMOD_Click(sender As Object, e As EventArgs) Handles btnMOD.Click
+
+        TextBox2.Text = TextBox1.Text + " MOD "
+        A = TextBox1.Text
+        TextBox1.Text = ""
+        pheptinh = "MOD"
+    End Sub
+
+    Private Sub btnpow_Click(sender As Object, e As EventArgs) Handles btnpow.Click
+        TextBox2.Text = TextBox1.Text + "^"
+        A = TextBox1.Text
+        TextBox1.Text = ""
+        pheptinh = "^"
+    End Sub
+
+    Private Sub btnchia_Click(sender As Object, e As EventArgs) Handles btnchia.Click
+        TextBox2.Text = TextBox1.Text + "/"
+        A = TextBox1.Text
+        TextBox1.Text = ""
+        pheptinh = "/"
+    End Sub
+    Private Sub btnbang_Click(sender As Object, e As EventArgs) Handles btnbang.Click
+        B = TextBox1.Text
+        TextBox2.Text += TextBox1.Text + "="
+        If pheptinh = "+" Then
+            kq = A + B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "-" Then
+            kq = A - B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "*" Then
+            kq = A * B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "/" And B = 0 Then
+            TextBox1.Text = "không chia được cho 0"
+        ElseIf pheptinh = "/" And B <> 0 Then
+            kq = A / B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "MOD" Then
+            kq = A Mod B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "^" Then
+            kq = A ^ B
+            TextBox1.Text = kq
+        ElseIf pheptinh = "sin" Then
+            Dim radians As Double = B * Math.PI / 180.0
+            kq = Math.Sin(radians)
+            TextBox1.Text = kq
+        ElseIf pheptinh = "cos" Then
+            Dim radians As Double = B * Math.PI / 180.0
+            kq = Math.Cos(radians)
+            TextBox1.Text = kq
+        ElseIf pheptinh = "sqrt" Then
+            kq = Math.Sqrt(B)
+            TextBox1.Text = kq
+        ElseIf pheptinh = "abs" Then
+            kq = Math.Abs(B)
+            TextBox1.Text = kq
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnCham_Click(sender As Object, e As EventArgs) Handles btnCham.Click
+        Dim check As String = True
+        For i As Integer = 0 To (TextBox1.Text).Length - 1
+            If (TextBox1.Text).Chars(i) = "." Then
+                check = False
+            End If
+        Next
+        If check = True Then
+            TextBox1.Text += "."
+        End If
+    End Sub
+End Class
